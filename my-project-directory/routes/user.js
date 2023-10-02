1d3f3d1ccf76e37cc2ff51bee922f789571af964
areignel <48579497+areignel@users.noreply.github.com>
2023-10-02 16:42:43 +0100

    Update user.js

---
+++ my-project-directory/routes/user.js
@@ -0,0 +1,11 @@
+// routes/user.js
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+
+// Route to register a new user
+router.post('/register', (req, res) => {
+  // Implement user registration logic here
+});
+
+module.exports = router;
